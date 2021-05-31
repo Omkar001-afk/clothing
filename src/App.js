4344ec8d66037acc3b184ca05da8291c595d0043
Omkar001-afk <2019omkar.patil@ves.ac.in>
2021-05-31 21:52:46 +0530

    created project with react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import HomePage from "./homepage.component";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./homepage.style.scss";
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">hats</h1>
+          <h2 className="subtitle">shop name</h2>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">jackets</h1>
+          <h2 className="subtitle">shop name</h2>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">shoes</h1>
+          <h2 className="subtitle">shop name</h2>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Male</h1>
+          <h2 className="subtitle">shop name</h2>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Female</h1>
+          <h2 className="subtitle">shop name</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default HomePage;
