756133a0934d53ee0cb4a76c677dabf80722adc5
Omkar001-afk <2019omkar.patil@ves.ac.in>
2021-06-15 17:00:15 +0530

    added header in app pages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pagess/homepage/homepage.jsx";
 import ShopPage from "./pagess/shops/shop.component.jsx";
-
+import Header from "./component/header/header.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
